47292ed9c35e1e09628aa49ea40301ee57fa3d46
Mike Branstein <mike.branstein@gmail.com>
2018-02-24 13:40:27 -0500

    add description and links to detail view

--- app/views/detail/detail.component.ts
+++ app/views/detail/detail.component.ts
@@ -5,6 +5,7 @@ import { Launch } from "../../models/launch";
 import { Links } from "../../models/links";
 import { LaunchService } from "../../services/launchService";
 
+import * as utils from "utils/utils";
 
 @Component({
     selector: "detail",
@@ -23,5 +24,9 @@ export class DetailComponent implements OnInit {
     ngOnInit(): void {
         const flightNumber = +this.route.snapshot.params["id"];
         this.launch = this.launchService.getLaunch(flightNumber);
+    }
+
+    onLinkTap(link: string) : void {
+        utils.openUrl(link);
     }
 }
